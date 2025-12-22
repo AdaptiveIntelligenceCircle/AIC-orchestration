1da3e81f8e55086f60e786ea00496dcd070dbd81
ndtribk <ndtribk@gmail.com>
2025-12-22 15:22:53 +0700

    isolate_node.sh

---
+++ examples/multi-node/script/isolate_node.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "[AIC] Isolating node-b..."
+../../bin/aicctl isolate node-b
+echo "[AIC] Node-b isolated."
---
+++ examples/multi-node/script/start_cluster.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo "[AIC] Starting cluster..."
+../../bin/aicctl start --cluster nodes/
