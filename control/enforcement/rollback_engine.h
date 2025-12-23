0ff543dcf74a9781d26c3485692b180b6a3c1474
ndtribk <ndtribk@gmail.com>
2025-12-23 22:53:10 +0700

    rollback_engine.h

--- control/enforcement/rollback_engine.h
+++ control/enforcement/rollback_engine.h
@@ -1,6 +1,10 @@
-#pragma once 
+#pragma once
+#include "../core/node_id.h"
+#include "../core/state_snapshot.h" 
+using namespace std; 
 
-class RollbackEngine {
+class RollbackEngine 
+{
 public:
     void rollback(NodeID node,
                   const StateSnapshot& snapshot);
