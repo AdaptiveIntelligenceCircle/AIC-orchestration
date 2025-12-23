e780f9761cb445478fa776ab6155b69a4d065845
ndtribk <ndtribk@gmail.com>
2025-12-23 22:40:03 +0700

    conflict_resolver.h

--- control/decision/conflict_resolver.h
+++ control/decision/conflict_resolver.h
@@ -1,12 +1,14 @@
 #pragma once 
 #include <iostream> 
 #include "control_decision.h"
+#include "../events/control_event.h"
+#include <vector> 
 
 using namespace std; 
 
 class ConflictResolver
 {
     public: 
-    ControlDecision resolve(/*const ControlEvent &event, 
-    const vector<ControlDecision> &candidates*/); 
+    ControlDecision resolve(const ControlEvent &event, 
+    const vector<ControlDecision> &candidates); 
 }; 
